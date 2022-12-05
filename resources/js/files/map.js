2fcc30a68308afa86bb2dd972afb394f29045bf1
Maxim Tresk <djtresk@gmail.com>
2022-12-05 23:21:48 +0500

    Load map on interaction

--- resources/js/files/map.js
+++ resources/js/files/map.js
@@ -1,54 +1,75 @@
-const map = document.querySelector('#map')
+window.onload = function () {
+    const map = document.querySelector('#map');
 
-if (map) {
-    function mapInit() {
-        ymaps.ready(init)
+    let options_map = {
+        once: true,
+        passive: true,
+        capture: true,
+    };
 
-        function init() {
-            // Создание карты.
-            var myMap = new ymaps.Map('map', {
-                // Координаты центра карты.
-                // Порядок по умолчанию: «широта, долгота».
-                // Чтобы не определять координаты центра карты вручную,
-                // воспользуйтесь инструментом Определение координат.
-                controls: [],
-                center: [57.161968520537066, 65.515336338623],
-                // Уровень масштабирования. Допустимые значения:
-                // от 0 (весь мир) до 19.
-                zoom: 16,
-            })
-
-            let myPlacemark = new ymaps.Placemark(
-                [57.161968520537066, 65.515336338623],
-                {},
-                {
-                    hasBalloon: false,
-                    hideIconOnBalloonOpen: false,
-                    // Необходимо указать данный тип макета.
-                    iconLayout: 'default#imageWithContent',
-                    // Своё изображение иконки метки.
-                    iconImageHref: '/img/map.svg',
-                    // Размеры метки.
-                    iconImageSize: [46, 32],
-                    // Смещение левого верхнего угла иконки относительно
-                    // её "ножки" (точки привязки).
-                    iconImageOffset: [-20, -15],
-                    // Смещение слоя с содержимым относительно слоя с картинкой.
-                    iconContentOffset: [0, 0],
-                }
-            )
-            myMap.geoObjects.add(myPlacemark)
-
-            // myMap.behaviors.disable('drag');
-            myMap.controls.remove('geolocationControl') // удаляем геолокацию
-            myMap.controls.remove('searchControl') // удаляем поиск
-            myMap.controls.remove('trafficControl') // удаляем контроль трафика
-            myMap.controls.remove('typeSelector') // удаляем тип
-            myMap.controls.remove('fullscreenControl') // удаляем кнопку перехода в полноэкранный режим
-            myMap.controls.remove('rulerControl') // удаляем контрол правил
-            myMap.behaviors.disable(['scrollZoom']) // отключаем скролл карты (опционально)
-        }
+    if (map) {
+        map.addEventListener('click', start_lazy_map, options_map);
+        map.addEventListener('mouseover', start_lazy_map, options_map);
+        map.addEventListener('touchstart', start_lazy_map, options_map);
+        map.addEventListener('touchmove', start_lazy_map, options_map);
     }
 
-    mapInit()
-}
+    let map_loaded = false;
+
+    function start_lazy_map() {
+        if (!map_loaded) {
+            let map_block = document.getElementById('ymap_lazy');
+            map_loaded = true;
+            map_block.setAttribute('src', map_block.getAttribute('data-src'));
+            map_block.removeAttribute('data-src');
+            setTimeout(() => {
+                if (map) {
+                    function mapInit() {
+                        ymaps.ready(init);
+
+                        function init() {
+                            // Создание карты.
+                            var myMap = new ymaps.Map('map', {
+                                controls: [],
+                                center: [57.161968520537066, 65.515336338623],
+                                zoom: 16,
+                            });
+
+                            let myPlacemark = new ymaps.Placemark(
+                                [57.161968520537066, 65.515336338623],
+                                {},
+                                {
+                                    hasBalloon: false,
+                                    hideIconOnBalloonOpen: false,
+                                    // Необходимо указать данный тип макета.
+                                    iconLayout: 'default#imageWithContent',
+                                    // Своё изображение иконки метки.
+                                    iconImageHref: '/img/map.svg',
+                                    // Размеры метки.
+                                    iconImageSize: [46, 32],
+                                    // Смещение левого верхнего угла иконки относительно
+                                    // её "ножки" (точки привязки).
+                                    iconImageOffset: [-20, -15],
+                                    // Смещение слоя с содержимым относительно слоя с картинкой.
+                                    iconContentOffset: [0, 0],
+                                }
+                            )
+                            myMap.geoObjects.add(myPlacemark);
+
+                            // myMap.behaviors.disable('drag');
+                            myMap.controls.remove('geolocationControl'); // удаляем геолокацию
+                            myMap.controls.remove('searchControl'); // удаляем поиск
+                            myMap.controls.remove('trafficControl'); // удаляем контроль трафика
+                            myMap.controls.remove('typeSelector'); // удаляем тип
+                            myMap.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+                            myMap.controls.remove('rulerControl'); // удаляем контрол правил
+                            myMap.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+                        }
+                    }
+
+                    mapInit();
+                }
+            }, 2000);
+        }
+    }
+};
--- resources/views/layout/map.blade.php
+++ resources/views/layout/map.blade.php
@@ -1,6 +1,13 @@
 <div class="map-block">
     <div class="map-block__container">
-        <div id="map" class="map-block__map map"></div>
+        <div id="map" class="map-block__map map">
+            <picture>
+                <source srcset="{{ asset('/img/map.webp') }} 1x, {{ asset('/img/map@2.webp') }} 2x" type="image/webp">
+                <img src="{{ asset('/img/map.jpg') }}"
+                     srcset="{{ asset('/img/map.jpg') }} 1x, {{ asset('/img/map@2.jpg') }} 2x"
+                     alt="Фоновое изображение" class="map__background"/>
+            </picture>
+        </div>
     </div>
 </div>
-<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=b1bae07c-a343-4699-89f6-bcca848765f3&_v=20221202013853"></script>
+<script id="ymap_lazy" data-src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=b1bae07c-a343-4699-89f6-bcca848765f3&_v=20220614214118"></script>
