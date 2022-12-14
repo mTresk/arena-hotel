38f80a8ae1f9582ee8e013c833fd079065c8e6c8
Maxim Tresk <djtresk@gmail.com>
2022-12-14 12:34:33 +0500

    Add vertical widget

--- resources/views/blocks/widget-horizontal.blade.php
+++ resources/views/blocks/widget-horizontal.blade.php
@@ -1,4 +1,5 @@
-<div class="hero__panel panel" id="panel"></div>
+<div class="hero__panel panel panel--horizontal" id="panel"></div>
+<div class="hero__panel panel panel--mobile" id="panel-mobile"></div>
 
 <script type="text/javascript">
     (function (k, o, t, e, l) {
@@ -20,6 +21,31 @@
             form: {
                 // Замените на ID html элемента, в который нужно вставить виджет
                 container: "panel",
+            }
+        }, "https://bookonline24.ru/");
+    });
+</script>
+
+<script type="text/javascript">
+    (function (k, o, t, e, l) {
+        l = document.createElement("script");
+        l.type = "text/javascript";
+        l.src = "https://bookonline24.ru/widget.js";
+        l.async = !0;
+        l.onload = l.onreadystatechange = function () {
+            e = this.readyState;
+            !o && (!e || e === "complete") && (o = 1) && k();
+        };
+        t = document.getElementsByTagName("script")[0];
+        t.parentNode.insertBefore(l, t);
+    })(function () {
+        HotelWidget.init({
+            // ID организации; ID html элемента нужно проставить ниже в поле container
+            id: "7d3c3ad8-43ce-4c3c-88d4-4127592fd516",
+            type: "verticalBlock",
+            form: {
+                // Замените на ID html элемента, в который нужно вставить виджет
+                container: "panel-mobile",
             }
         }, "https://bookonline24.ru/");
     });
